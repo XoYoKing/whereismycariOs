5753091b7c7e514c7790212702dc299f345e6393
Francisco Buitrago Pavon <Buitrago@MacBook-Pro-de-Francisco.local>
2013-05-05 18:59:50 +0200

    Initial Commit

---
+++ whereismycar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  whereismycar
+//
+//  Created by Francisco Buitrago Pavon on 05/05/13.
+//  Copyright (c) 2013 Francisco Buitrago Pavon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ whereismycar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  whereismycar
+//
+//  Created by Francisco Buitrago Pavon on 05/05/13.
+//  Copyright (c) 2013 Francisco Buitrago Pavon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
